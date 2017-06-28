d4a5906bc4601ae1bfc5c6fab00be230681b4e17
shabbir1993 <syed_shabbir_haider@hotmail.com>
2017-06-28 21:56:14 +0500

    updated the Urls

--- src/App.js
+++ src/App.js
@@ -2,36 +2,42 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-let USER_ID = 0;
-const USER = "https://api.twitch.tv/kraken/users/";
+const USER = "https://api.twitch.tv/kraken/channels/";
 const STREAM = "https://api.twitch.tv/kraken/streams/";
-let USERNAME = "dallas";
+let USERNAME = "B0aty";
 const CLIENT_ID = "?&client_id=rcfc26iizksue4oaw9o3zl5b61m5k9";
+let users = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"]
+//let user_results; 
 
 class App extends Component {
   constructor(props){
     super(props);
 
     this.fetchStreams = this.fetchStreams.bind(this);
+    this.getUserStreaming = this.getUserStreaming.bind(this);
   }
 
   fetchStreams(){
       fetch(`${USER}${USERNAME}${CLIENT_ID}`)
       .then(response => response.json()).then(response =>
       {
-        USER_ID = response._id;
         console.log(response);
-        fetch(`${STREAM}${USER_ID}${CLIENT_ID}`)
-        .then(response => response.json)
-        .then(response => 
-              {
-                response.stream ?
-                  console.log("User Online"):console.log("User Offline")
-              }
-            )
-      })
+        this.getUserStreaming(USERNAME);
+      });    
   }
 
+  getUserStreaming(USERNAME){ 
+    fetch(`${STREAM}${USERNAME}${CLIENT_ID}`)
+        .then(response => response.json())
+        .then(response => 
+          {
+            alert(response);
+            console.log(response);
+            response.stream ?
+              console.log("User Online"):console.log("User Offline")
+          }
+        )
+  }           
 
   render() {
     return (
