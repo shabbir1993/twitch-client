8a334f40ff4623ee75563323c12021fdfb81694d
shabbir1993 <syed_shabbir_haider@hotmail.com>
2017-07-01 09:56:12 +0500

    fixed issue with state

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-let status = "";
+//let status = "";
 const CHANNEL = "https://api.twitch.tv/kraken/channels/";
 //const STREAM = "https://api.twitch.tv/kraken/streams/";
 const CLIENT_ID = "?&client_id=rcfc26iizksue4oaw9o3zl5b61m5k9";
@@ -20,67 +20,34 @@ class App extends Component {
     }
 
     this.fetchChannel = this.fetchChannel.bind(this);
-    this.randomOnCheck = this.randomOnCheck.bind(this);
-    //this.fetchStream = this.fetchStream.bind(this);
-    //this.fetchStreams = this.fetchStreams.bind(this);
-    //this.getUserStreaming = this.getUserStreaming.bind(this);
-    //this.bulkfetch = this.bulkfetch.bind(this);
+    
+  }
+
+
+  getInitialState(){
+    return {
+      result:bulkResults
+    }
   }
 
   fetchChannel(username){
     fetch(`${CHANNEL}${username}${CLIENT_ID}`).then(
       response => response.json()
     ).then(data =>
-      bulkResults.push(data)
+      {
+        bulkResults.push(data)
+        this.setState({
+          result:bulkResults
+        })
+      }
       )
   }
-
-  /*fetchStream(username){
-    fetch(`${STREAM}${username}${CLIENT_ID}`).then(
-      response => response.json()
-    ).then(
-      data=> {
-        data.stream ?
-          console.log(data)
-          :
-          status = "This user is Offline"
-
-      }
-    )
-  }*/
   
-  randomOnCheck(){
+  componentWillMount(){
     users.map(item => this.fetchChannel(item));
+    console.log("Before Render")
     console.log(bulkResults);
-    this.setState({
-      result:bulkResults
-    })
-    console.log("Button Clicked")
-  }
-
-  /*fetchStreams(){
-      fetch(`${CHANNEL}${USERNAME}${CLIENT_ID}`)
-      .then(response => response.json()).then(response =>
-      {
-        console.log(response);
-        this.getUserStreaming(USERNAME);
-      });    
-  }
-*/
-  /*getUserStreaming(USERNAME){ 
-    fetch(`${STREAM}${USERNAME}${CLIENT_ID}`)
-        .then(response => response.json())
-        .then(response => 
-          {
-            alert(response);
-            console.log(response);
-            response.stream ? 
-              status = response 
-              :console.log("User Offline")
-              console.log(status);
-          }  
-        )
-  }       */    
+  }   
 
   render() {
     return (
@@ -89,24 +56,21 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Twitch Streamers</h2>
         </div>
-        <p className="App-intro">
-          
-          <button onClick = {this.randomOnCheck}> Get Streams </button>
           {
             this.state.result ?
-            this.state.result.map(item =>
-            <div key = {item._id}>
-              <ul>
-                <li>
-                  <img src={item.logo} width = "50px" height = "50px" alt=""/>
-                  <a target="blank" href={item.url}>{item.display_name}</a>
-                </li>
-              </ul>
-              </div>
-            )
-            : ""
+              this.state.result.map(item =>
+              <div key = {item._id}>
+                <ul>
+                  <li>
+                    <img src={item.logo} width = "50px" height = "50px" alt=""/>
+                    <a target="blank" href={item.url}>{item.display_name}</a>
+                  </li>
+                </ul>
+                </div>
+              )
+              : ""
           }
-        </p>
+        
       </div>
     );
   }
